c15e8394268027af12d72e3fd70085d827f777fc
martijn.niji@gmail.com <martijn.niji@gmail.com@f68477e9-1835-0410-8f7c-e127d972d001>
2008-10-12 10:54:28 +0000

    frontend/backend working

---
+++ branches/dev_nosplit/admin/index.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+   Frog CMS - Content Management Simplified. <http://www.madebyfrog.com>
+   Copyright (C) 2008 Philippe Archambault <philippe.archambault@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//  Constantes  --------------------------------------------------------------
+
+define('FROG_VERSION', '0.9.4');
+
+define('FROG_ROOT', dirname(__FILE__).'/..');
+define('CORE_ROOT', FROG_ROOT.'/frog');
+
+define('APP_PATH',  CORE_ROOT.'/app');
+
+define('SESSION_LIFETIME', 3600);
+define('REMEMBER_LOGIN_LIFETIME', 1209600); // two weeks
+
+define('DEFAULT_CONTROLLER', 'page');
+define('DEFAULT_ACTION', 'index');
+
+define('COOKIE_PATH', '/');
+define('COOKIE_DOMAIN', '');
+define('COOKIE_SECURE', false);
+
+include FROG_ROOT.'/config.php';
+
+define('BASE_URL', URL_PUBLIC . ADMIN_DIR . (USE_MOD_REWRITE ? '/': '/?/'));
+
+include CORE_ROOT.'/Framework.php';
+
+
+//  Database connection  -----------------------------------------------------
+
+if (USE_PDO)
+{
+    $__FROG_CONN__ = new PDO(DB_DSN, DB_USER, DB_PASS);
+    if ($__FROG_CONN__->getAttribute(PDO::ATTR_DRIVER_NAME) == 'mysql')
+        $__FROG_CONN__->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+}
+else
+{
+    require_once CORE_ROOT . '/libraries/DoLite.php';
+    $__FROG_CONN__ = new DoLite(DB_DSN, DB_USER, DB_PASS);
+}
+
+Record::connection($__FROG_CONN__);
+Record::getConnection()->exec("set names 'utf8'");
+
+
+//  Initialize  --------------------------------------------------------------
+
+Setting::init();
+
+use_helper('I18n');
+I18n::setLocale(Setting::get('language'));
+
+Plugin::init();
+
+//  Get controller and action and execute  -----------------------------------
+
+Dispatcher::dispatch(null, Setting::get('default_tab'));
---
+++ branches/dev_nosplit/frog/plugins/comment/views/index.php
@@ -0,0 +1,106 @@
+<h1><?php echo __('Comments'); ?></h1>
+<?php error_reporting(E_ALL); ?>
+<div id="comments-def">
+    <div class="comment"><?php echo __('Comments'); ?></div>
+    <div class="modify"><?php echo __('Modify'); ?></div>
+</div>
+<?php
+global $__FROG_CONN__;
+$sql = "SELECT * FROM ".TABLE_PREFIX."comment WHERE is_approved = 1";
+$stmt = $__FROG_CONN__->prepare($sql);
+$stmt->execute();
+
+$comments_count = $stmt->rowCount();
+
+if (isset($page)) {
+    $CurPage = $page;
+} else {
+    $CurPage = 0;
+}
+$sql = "SELECT * FROM ".TABLE_PREFIX."setting WHERE name = 'rowspage'";
+$stmt = $__FROG_CONN__->prepare($sql);
+$stmt->execute();
+$rowspage = $stmt->fetchObject();
+$rowspage = $rowspage->value;
+
+$start = $CurPage * $rowspage;
+
+$totalrecords = $comments_count;
+$sql = "SELECT comment.is_approved, comment.id, comment.page_id, comment.author_name, comment.body, comment.created_on, page.title FROM " .
+    TABLE_PREFIX . "comment AS comment, " . TABLE_PREFIX .
+    "page AS page WHERE comment.is_approved = 1 AND comment.page_id = page.id LIMIT " . $start . "," . $rowspage;
+
+$stmt = $__FROG_CONN__->prepare($sql);
+$stmt->execute();
+$lastpage = ceil($totalrecords / $rowspage);
+if($comments_count <= $rowspage) { $lastpage = 0; } else { $lastpage = abs($lastpage - 1); }
+?>
+<?php if (count($comments)): ?>
+<ol id="comments">
+<?php while ($comment = $stmt->fetchObject()): ?>
+    <li class="<?php echo odd_even(); ?> moderate">
+          <a href="<?php echo get_url('plugin/comment/edit/' . $comment->id); ?>"><b><?php echo
+$comment->title; ?></b></a>
+          <p><?php echo $comment->body; ?></p>
+          <div class="infos">
+              <?php echo date('D, j M Y', strtotime($comment->created_on)); ?> &#8212; 
+              <a href="<?php echo get_url('plugin/comment/delete/' . $comment->
+id); ?>" onclick="return confirm('<?php echo
+__('Are you sure you wish to delete it?'); ?>');"><?php echo
+__('Delete'); ?></a> | 
+<?php if ($comment->is_approved): ?>
+              <a href="<?php echo get_url('plugin/comment/unapprove/' . $comment->
+id); ?>"><?php echo
+__('Reject'); ?></a>
+<?php else: ?>
+              <a href="<?php echo get_url('plugin/comment/approve/' . $comment->
+id); ?>"><?php echo
+__('Approve'); ?></a>
+<?php endif; ?>
+          </div>
+      </li>
+<?php endwhile; ?>
+</ol>
+<?php else: ?>
+<h3><?php echo __('No comments found.'); ?></h3>
+<?php endif; ?>
+<br />
+<div class="pagination">
+<?php
+            if ($CurPage == $lastpage) {
+                $next = '<span class="disabled">Next Page</span>';
+            } else {
+                $nextpage = $CurPage + 1;
+                $next = '<a href="' . get_url('plugin/comment/index/') . '' . $nextpage .
+                    '">Next Page</a>';
+
+            }
+            if ($CurPage == 0) {
+               $prev = '<span class="disabled">Previous Page</span>';
+            } else {
+                $prevpage = $CurPage - 1;
+                $prev = '<a href="' . get_url('plugin/comment/index/') . '' . $prevpage .
+                    '">Previous Page</a>';
+            }
+            if ($CurPage != 0) {
+                echo "<a href=" . get_url('plugin/comment/index/') . "0>First Page</a>\n ";
+            }
+            else {
+            	echo "<span class=\"disabled\">First Page</span>";
+            }
+            echo $prev;
+            for ($i = 0; $i <= $lastpage; $i++) {
+                if ($i == $CurPage)
+                    echo '<span class="current">'.$i.'</span';
+                else
+                    echo " <a href=" . get_url('plugin/comment/index/') . "$i>$i</a>\n";
+            }
+            echo $next;
+            if ($CurPage != $lastpage) {
+                echo "\n<a href=" . get_url('plugin/comment/index/') . "$lastpage>Last Page</a>&nbsp&nbsp;";
+            }
+            else {
+            	echo "<span class=\"disabled\">Last Page</span>";
+            }
+?>
+</div>
